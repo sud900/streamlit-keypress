4f7a113da02cd234d95bc6c7f39860c8f9ac3238
Sudarsan Mandal <sudarsan.mandal@ecolab.com>
2026-07-07 12:58:23 +0530

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="streamlit-keypress",
-    version="0.1.0",
+    version="0.1.1",
     author="Sudarsan",
     author_email="sudarsan900@gmail.com",
     description="A Streamlit component to capture keyboard events",
